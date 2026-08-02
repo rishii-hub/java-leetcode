05bc5dd848b5db8ebe00419b8fa14bf9fa40a8d4
Vivek Sheel Rishi <174921501+rishii-hub@users.noreply.github.com>
2026-08-02 09:32:57 +0530

    Time: 1 ms (93.75%), Space: 42.3 MB (82.61%) - LeetHub-Neo

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int div = 0;
+        int ndiv = 0;
+
+        for(int i = 1; i <= n ; i++){
+            if( i % m == 0){
+                div += i;
+            }
+            else{
+                ndiv += i;
+            }
+        }
+        return ndiv - div;
+    }
+}
